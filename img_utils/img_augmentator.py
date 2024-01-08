916c57cd34272c3e95448c4a0acccd3362cb1853
nhattien <nhattien1245@gmail.com>
2024-01-08 21:43:09 +0700

    additional images for classification (augmented by rotation)

--- img_utils/img_augmentator.py
+++ img_utils/img_augmentator.py
@@ -2,11 +2,12 @@ import os
 from os import listdir
 from PIL import Image
 
-folder_dir = "helmet_classify_img"
+folder_dir = "../dataset/helmet_classify_img"
 for images_path in os.listdir(folder_dir):
-    im = Image.open(f"helmet_classify_img/{images_path}")
+    im = Image.open(f"../dataset/helmet_classify_img/{images_path}")
+    im = im.convert('RGB')
     left_tilt_im = im.rotate(30)
     right_tilt_im = im.rotate(-30)
     origin_name = images_path.split('.')[0]
-    left_tilt_im.save(f'helmet_classify_img/left_titl_{origin_name}.jpg')
-    right_tilt_im.save(f'helmet_classify_img/right_titl_{origin_name}.jpg')
+    left_tilt_im.save(f'../dataset/helmet_classify_img/left_titl_{origin_name}.jpg')
+    right_tilt_im.save(f'../dataset/helmet_classify_img/right_titl_{origin_name}.jpg')
